ec8a6c4705735bc612d4d04de5291883c4b5979e
Guang <guangzhao@tamu.edu>
2019-02-21 15:34:29 -0600

    update

--- paper-3d-grid-and-IRS/ehvi_montecarlo.h
+++
@@ -1,5 +0,0 @@
-//include this to use the Monte Carlo EHVI calculation scheme.
-#include <deque>
-#include "newhelper.h"
-
-double ehvi3d_montecarlo(deque<individual*> P, double r[], double mu[], double s[]);
--- paper-3d-grid-and-IRS/helper.cc
+++
@@ -1,5 +0,0 @@
-#include "newhelper.h"
-#include "ehvi_consts.h"
-#include <math.h>
-
-
